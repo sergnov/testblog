05388f40192a47d6f5554a8005d66879825b713c
sergnov <zkgoldenman@gmail.com>
2016-12-02 20:33:39 +0300

    Добавил реализацию отправки почты при добавлении поста с использованием тестового почтового бэкэнда.

--- testblog/settings.py
+++ testblog/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Email backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- testblogapp/helpers.py
+++ testblogapp/helpers.py
@@ -1,4 +1,14 @@
+from django.core import mail
+
 def worker(subscribers, post):
     # print(dir(subscribers[0]))
-    for us in subscribers:
-        print("Send email from",us.user, "to", us.blog)
+    
+        # print("Send email from",us.user, "to", us.blog)
+        
+    with mail.get_connection() as connection:
+        for us in subscribers:
+            subject = "Post  added: "+post.title
+            body = '''
+                See http://testblog-workdb.rhcloud.com/fulllength?id={0}
+            '''.format(post.id)
+            mail.EmailMessage(subject, body, 'no-reply@testblog-workdb.rhcloud.com', [us.blog.email], connection=connection).send()
